f5649150f3162b94aeabae5e60e959213381fdfa
igips <igor.szepietowski@gmail.com>
2022-03-09 20:51:14 +0100

    Add divs

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,23 @@ import "../styles/Home.css";
 function Home() {
   return (
     <main>
-        <section id="home-section"></section>
+        <section id="home-section">
+            <div id="home-left-div">
+                <div id="home-left-inner">
+                   
+
+                </div>
+                
+            </div>    
+
+            <div id="home-right-div">
+
+                
+            </div>
+            
+            
+            
+        </section>
     </main>
 
   );
