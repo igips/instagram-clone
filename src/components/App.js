f89b95386090f69994dfb759185f99d7baeac84b
igips <igor.szepietowski@gmail.com>
2022-03-09 19:40:21 +0100

    Add id to section and Add home

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import "../styles/App.css";
 import Nav from "./Nav.js";
+import Home from "./Home.js";
 
 
 function App() {
   return (
-    <section>
+    <section id="top-section">
       <Nav></Nav>
-
-
-
+      <Home></Home>
+      
+      
 
     </section>
   );
