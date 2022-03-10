efb7a6e977188ce52c646713fc40ab3a02b4d7a8
igips <igor.szepietowski@gmail.com>
2022-03-10 20:33:41 +0100

    Add comment section

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -141,7 +141,7 @@ function PictureCard() {
 									<span className="name-span in-coms">kristencor</span>
 									<span className="comment-span">Siala bala puku buku</span>
 								</div>
-                                <div className="like-comment-div">
+								<div className="like-comment-div">
 									<svg
 										aria-label="Like"
 										color="#262626"
@@ -177,6 +177,29 @@ function PictureCard() {
 							</div>
 						</div>
 					</div>
+					<div className="added-div">6 DAYS AGO</div>
+					<section className="add-comment-section">
+						<div className="add-comment-section-inner">
+							<form className="comment-form">
+								<div className="emoji-div">
+									<svg
+										aria-label="Emoji"
+										color="#262626"
+										fill="#262626"
+										height="24"
+										role="img"
+										viewBox="0 0 24 24"
+										width="24"
+									>
+										<path d="M15.83 10.997a1.167 1.167 0 101.167 1.167 1.167 1.167 0 00-1.167-1.167zm-6.5 1.167a1.167 1.167 0 10-1.166 1.167 1.167 1.167 0 001.166-1.167zm5.163 3.24a3.406 3.406 0 01-4.982.007 1 1 0 10-1.557 1.256 5.397 5.397 0 008.09 0 1 1 0 00-1.55-1.263zM12 .503a11.5 11.5 0 1011.5 11.5A11.513 11.513 0 0012 .503zm0 21a9.5 9.5 0 119.5-9.5 9.51 9.51 0 01-9.5 9.5z"></path>
+									</svg>
+								</div>
+								<textarea placeholder="Add a comment…" className="add-comment-text-area"></textarea>
+								
+								<div className="post-div">Post</div>
+							</form>
+						</div>
+					</section>
 				</div>
 			</div>
 		</article>
