47151e9486b2983399793c28cf2548ba8f17d386
igips <igor.szepietowski@gmail.com>
2022-03-10 17:10:22 +0100

    Add comment section

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -14,7 +14,7 @@ function PictureCard() {
 									<canvas height="42" width="42"></canvas>
 									<span className="avatar-span">
 										<img
-                                            alt=""
+											alt=""
 											className="card-avatar-img"
 											data-testid="user-avatar"
 											draggable="false"
@@ -47,12 +47,138 @@ function PictureCard() {
 					</div>
 				</div>
 			</div>
-            <div className="picture-div">
-                <div className="picture-div-inner">
-                    <img className="main-picture" src={testPic} alt="" />
-                </div>
-                
-            </div>
+			<div className="picture-div">
+				<div className="picture-div-inner">
+					<img className="main-picture" src={testPic} alt="" />
+				</div>
+			</div>
+			<div className="comments-div">
+				<div className="comments-inner-div">
+					<section className="icons-in-comments-section">
+						<span className="comms-icon-span">
+							<svg
+								aria-label="Like"
+								color="#262626"
+								fill="#262626"
+								height="24"
+								role="img"
+								viewBox="0 0 24 24"
+								width="24"
+							>
+								<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+							</svg>
+						</span>
+						<span className="comms-icon-span">
+							<svg
+								aria-label="Comment"
+								color="#262626"
+								fill="#262626"
+								height="24"
+								role="img"
+								viewBox="0 0 24 24"
+								width="24"
+							>
+								<path
+									d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z"
+									fill="none"
+									stroke="currentColor"
+									strokeLinejoin="round"
+									strokeWidth="2"
+								></path>
+							</svg>
+						</span>
+						<span className="comms-icon-span">
+							<svg
+								aria-label="Share Post"
+								color="#262626"
+								fill="#262626"
+								height="24"
+								role="img"
+								viewBox="0 0 24 24"
+								width="24"
+							>
+								<line
+									fill="none"
+									stroke="currentColor"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									x1="22"
+									x2="9.218"
+									y1="3"
+									y2="10.083"
+								></line>
+								<polygon
+									fill="none"
+									points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
+									stroke="currentColor"
+									strokeLinejoin="round"
+									strokeWidth="2"
+								></polygon>
+							</svg>
+						</span>
+					</section>
+					<section className="number-of-likes-section">
+						<div className="number-of-likes-div">
+							<span className="likes-span">25,350 likes</span>
+						</div>
+					</section>
+					<div className="pic-comments-div">
+						<div className="pic-comments-inner-div">
+							<div className="comment-line-div">
+								<div>
+									<span className="name-span in-coms">blabeksrapek</span>
+									<span className="comment-span">
+										Drop a if you’re ready to go on this ride with us this month
+									</span>
+								</div>
+							</div>
+							<div className="comment-line-div">
+								<span className="view-all-coms-span">View all 568 comments</span>
+							</div>
+
+							<div className="comment-line-div">
+								<div className="comment-line-inner-div">
+									<span className="name-span in-coms">kristencor</span>
+									<span className="comment-span">Siala bala puku buku</span>
+								</div>
+                                <div className="like-comment-div">
+									<svg
+										aria-label="Like"
+										color="#262626"
+										fill="#262626"
+										height="12"
+										role="img"
+										viewBox="0 0 24 24"
+										width="12"
+									>
+										<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+									</svg>
+								</div>
+							</div>
+
+							<div className="comment-line-div">
+								<div className="comment-line-inner-div">
+									<span className="name-span in-coms">labrakonks</span>
+									<span className="comment-span">Tererer bakbuka</span>
+								</div>
+								<div className="like-comment-div">
+									<svg
+										aria-label="Like"
+										color="#262626"
+										fill="#262626"
+										height="12"
+										role="img"
+										viewBox="0 0 24 24"
+										width="12"
+									>
+										<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+									</svg>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</article>
 	);
 }
