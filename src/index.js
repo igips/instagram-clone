8ff21e9bce5cc59a851952625e168eae0236cafc
igips <igor.szepietowski@gmail.com>
2022-03-11 19:04:53 +0100

    Add firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import "./styles/index.css";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDft-ERJnOSLsjY01BIvYaXN3BSQFcVvos",
+  authDomain: "instagram-clone-33934.firebaseapp.com",
+  projectId: "instagram-clone-33934",
+  storageBucket: "instagram-clone-33934.appspot.com",
+  messagingSenderId: "776008847798",
+  appId: "1:776008847798:web:2b67fd832c1478c19ab117"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
