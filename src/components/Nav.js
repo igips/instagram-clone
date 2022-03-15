b2a189918d4fb786f8cbd992e4cc753a70605960
igips <igor.szepietowski@gmail.com>
2022-03-15 20:58:48 +0100

    Add fill to icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,6 +23,7 @@ function Nav() {
 									width="24"
 								>
 									<path
+										id="home-icon"
 										className="svh-path"
 										d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
 										fill="black"
