bb8b8f4e3267bc538c0244b051f108d672a51751
igips <igor.szepietowski@gmail.com>
2022-03-15 16:25:27 +0100

    Write signup logic

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,80 @@
 import favi from "../img/favicon.jpg";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore";
 
 function Modal() {
 	function checkIfPasswordMatch() {
-		if (document.getElementById("password-input-rep").value !== document.getElementById("password-input").value) {
-			document.getElementById("password-input-rep").setCustomValidity("Password Must be Matching.");
+		const passwordInput = document.getElementById("password-input-rep");
+		const passwordInput2 = document.getElementById("password-input");
+
+		if (passwordInput.value !== passwordInput2.value) {
+			passwordInput.setCustomValidity("Password Must be Matching.");
 		} else {
-			document.getElementById("password-input-rep").setCustomValidity("");
+			passwordInput.setCustomValidity("");
 		}
+	}
+
+	function clearSignUpInputs(inp) {
+		document.getElementById("password-input").value = "";
+		document.getElementById("email-input").value = "";
+		document.getElementById("password-input-rep").value = "";
+		document.getElementById("username-input").value = "";
+	}
+
+	function clearSigInInputs() {
+		document.getElementById("login-username-input").value = "";
+		document.getElementById("login-password-input").value = "";
+	}
+
+	async function usernameAvailable() {
+		const userName = document.getElementById("username-input");
+		const data = await getDocs(collection(getFirestore(), "usernames"));
+		let check = true;
+
+		data.forEach((doc) => {
+			if (doc.data().username === userName.value) {
+				check = false;
+			}
+		});
+
+		return Boolean(check);
+	}
+
+	function signUp(e) {
+		e.preventDefault();
+
+		const email = document.getElementById("email-input");
+		const pass = document.getElementById("password-input-rep");
+		const userName = document.getElementById("username-input");
+		const formSubmitButton = document.getElementById("submit-sign-up-form-button");
+        const signUpModal = document.getElementById("registerModal");
+
+		usernameAvailable().then((avail) => {
+			if (avail) {
+				createUserWithEmailAndPassword(getAuth(), email.value, pass.value)
+					.then((userCredential) => {
+						const user = userCredential.user;
+
+						addDoc(collection(getFirestore(), "usernames"), {
+							uid: user.uid,
+							username: userName.value,
+						});
+
+                        signUpModal.style.display = "none";
+						clearSignUpInputs();
+
+					})
+					.catch((error) => {
+						if (error.code === "auth/email-already-in-use") {
+							email.setCustomValidity("Email already used!");
+							formSubmitButton.click();
+						}
+					});
+			} else {
+				userName.setCustomValidity("Username already taken!");
+				formSubmitButton.click();
+			}
+		});
 	}
 
 	return (
@@ -14,28 +82,33 @@ function Modal() {
 			<div id="registerModal" className="modal">
 				<div className="modal-content">
 					<span
-						onClick={() => (document.getElementById("registerModal").style.display = "none")}
+						onClick={() => {
+							document.getElementById("registerModal").style.display = "none";
+							clearSignUpInputs();
+						}}
 						className="close-modal"
 					>
 						&times;
 					</span>
 					<img className="img-modal" src={favi} alt="" />
 					<p>Sign Up</p>
-					<form onSubmit={(e) => e.preventDefault()} id="reg-form">
+					<form onSubmit={(e) => signUp(e)} id="reg-form">
 						<input
-							required
-							id="email-input"
-							className="input-modal"
-							type="email"
-							placeholder="Email"
-						/>
-                        <input
 							maxLength="15"
 							required
 							id="username-input"
 							className="input-modal"
 							type="text"
 							placeholder="Username"
+							onInput={() => document.getElementById("username-input").setCustomValidity("")}
+						/>
+						<input
+							required
+							id="email-input"
+							className="input-modal"
+							type="email"
+							placeholder="Email"
+							onInput={() => document.getElementById("email-input").setCustomValidity("")}
 						/>
 						<input
 							id="password-input"
@@ -43,6 +116,7 @@ function Modal() {
 							placeholder="Password"
 							required
 							type="password"
+							minLength="6"
 							onInput={() => checkIfPasswordMatch()}
 						/>
 						<input
@@ -50,15 +124,19 @@ function Modal() {
 							className="input-modal"
 							placeholder="Repeat Password"
 							required
+							minLength="6"
 							type="password"
 							onInput={() => checkIfPasswordMatch()}
 						/>
-						<button className="modal-button">Sign Up</button>
+						<button id="submit-sign-up-form-button" className="modal-button">
+							Sign Up
+						</button>
 					</form>
 					<p
 						onClick={() => {
 							document.getElementById("registerModal").style.display = "none";
 							document.getElementById("loginModal").style.display = "flex";
+							clearSignUpInputs();
 						}}
 						id="already-sign-ip"
 					>
@@ -69,7 +147,10 @@ function Modal() {
 			<div id="loginModal" className="modal">
 				<div className="modal-content">
 					<span
-						onClick={() => (document.getElementById("loginModal").style.display = "none")}
+						onClick={() => {
+							document.getElementById("loginModal").style.display = "none";
+							clearSigInInputs();
+						}}
 						className="close-modal"
 					>
 						&times;
@@ -82,7 +163,7 @@ function Modal() {
 							required
 							id="login-username-input"
 							className="input-modal"
-							type="text"
+							type="email"
 							placeholder="Email"
 						/>
 						<input
@@ -96,8 +177,9 @@ function Modal() {
 					</form>
 					<p
 						onClick={() => {
-                            document.getElementById("loginModal").style.display = "none";
+							document.getElementById("loginModal").style.display = "none";
 							document.getElementById("registerModal").style.display = "flex";
+							clearSigInInputs();
 						}}
 						id="already-login-ip"
 					>
