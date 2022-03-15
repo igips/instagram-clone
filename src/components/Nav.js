3fe5c566e0e1ae6f41eb37c746d1cc33f6490f06
igips <igor.szepietowski@gmail.com>
2022-03-15 17:10:26 +0100

    Change icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -25,7 +25,7 @@ function Nav() {
 									<path
 										className="svh-path"
 										d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
-										fill="none"
+										fill="black"
 										stroke="currentColor"
 										strokeLinejoin="round"
 										strokeWidth="2"
