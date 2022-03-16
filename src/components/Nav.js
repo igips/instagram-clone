7217657ee8487cd6cef9665923f7ad44874c6fa4
igips <igor.szepietowski@gmail.com>
2022-03-16 19:29:40 +0100

    Add functions for icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,224 @@
 import "../styles/Nav.css";
 import ava from "../img/ava.jpeg";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
 
 function Nav() {
+	const [avatar, setAvatar] = useState(0);
+
+	onAuthStateChanged(getAuth(), (user) => {
+		if (user) {
+			setAvatar(1);
+		} else {
+			setAvatar(0);
+		}
+	});
+
+	function profileIcon() {
+		const user = getAuth().currentUser;
+
+		if (user) {
+			return (
+				<span onClick={() => avatarIcon()} role="link" tabIndex="0" style={{ width: "24px", height: "24px" }}>
+					<img id="ava" alt="" crossOrigin="anonymous" draggable="false" src={ava} />
+				</span>
+			);
+		} else {
+			return (
+				<svg
+					onClick={() => profileIconCheck()}
+					stroke="currentColor"
+					fill="currentColor"
+					strokeWidth="0"
+					viewBox="0 0 512 512"
+					height="24"
+					width="24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill="none"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="40"
+						d="M344 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z"
+					></path>
+					<path
+						fill="none"
+						strokeMiterlimit="10"
+						strokeWidth="40"
+						d="M256 304c-87 0-175.3 48-191.64 138.6C62.39 453.52 68.57 464 80 464h352c11.44 0 17.62-10.48 15.65-21.4C431.3 352 343 304 256 304z"
+					></path>
+				</svg>
+			);
+		}
+	}
+
+	function profileIconCheck() {
+		const user = getAuth().currentUser;
+
+		if (user) {
+		} else {
+			showSignInModal();
+		}
+	}
+
+	function avatarIcon() {
+		const user = getAuth().currentUser;
+
+		if (user) {
+			const svgDiv = document.getElementById("message-icon-svg");
+			const homeSvg = Array.from(document.getElementsByClassName("home-icon"));
+			const notifiIcon = Array.from(document.getElementsByClassName("notification-svg"));
+			const searchIconDiv = document.getElementById("search-icon-div");
+
+			svgDiv.innerHTML =
+				'<line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line> <polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon>';
+
+			homeSvg.forEach((icon) => {
+				icon.style.fill = "none";
+			});
+
+			notifiIcon.forEach((icon) => {
+				icon.innerHTML =
+					'<svg aria-label="Activity" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>';
+			});
+
+			searchIconDiv.innerHTML =
+				'<svg aria-label="Search &amp; Explore" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>';
+		} else {
+		}
+	}
+
+	function messageIcon() {
+		const user = getAuth().currentUser;
+
+		if (user) {
+			const svgDiv = document.getElementById("message-icon-svg");
+			const homeSvg = Array.from(document.getElementsByClassName("home-icon"));
+			const notifiIcon = Array.from(document.getElementsByClassName("notification-svg"));
+			const searchIconDiv = document.getElementById("search-icon-div");
+
+			svgDiv.innerHTML =
+				'<path d="M22.91 2.388a.69.69 0 00-.597-.347l-20.625.002a.687.687 0 00-.482 1.178L7.26 9.16a.686.686 0 00.778.128l7.612-3.657a.723.723 0 01.937.248.688.688 0 01-.225.932l-7.144 4.52a.69.69 0 00-.3.743l2.102 8.692a.687.687 0 00.566.518.655.655 0 00.103.008.686.686 0 00.59-.337L22.903 3.08a.688.688 0 00.007-.692" fill-rule="evenodd"></path>';
+
+			homeSvg.forEach((icon) => {
+				icon.style.fill = "none";
+			});
+
+			notifiIcon.forEach((icon) => {
+				icon.innerHTML =
+					'<svg aria-label="Activity" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>';
+			});
+
+			searchIconDiv.innerHTML =
+				'<svg aria-label="Search &amp; Explore" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>';
+		} else {
+			showSignInModal();
+		}
+	}
+
+	function homeIcon() {
+		const user = getAuth().currentUser;
+		const homeSvg = Array.from(document.getElementsByClassName("home-icon"));
+		const searchIconDiv = document.getElementById("search-icon-div");
+
+		homeSvg.forEach((icon) => {
+			icon.style.fill = "black";
+		});
+
+		searchIconDiv.innerHTML =
+			'<svg aria-label="Search &amp; Explore" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>';
+
+			
+
+		if (user) {
+			const svgDiv = document.getElementById("message-icon-svg");
+			const notifiIcon = Array.from(document.getElementsByClassName("notification-svg"));
+
+			svgDiv.innerHTML =
+				'<line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line> <polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon>';
+
+			notifiIcon.forEach((icon) => {
+				icon.innerHTML =
+					'<svg aria-label="Activity" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>';
+			});
+		} else {
+		}
+	}
+
+	function notificationIcon() {
+		const user = getAuth().currentUser;
+
+		if (user) {
+			const homeSvg = Array.from(document.getElementsByClassName("home-icon"));
+			const svgDiv = document.getElementById("message-icon-svg");
+			const notifiIcon = Array.from(document.getElementsByClassName("notification-svg"));
+			const searchIconDiv = document.getElementById("search-icon-div");
+
+			svgDiv.innerHTML =
+				'<line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line> <polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon>';
+
+			homeSvg.forEach((icon) => {
+				icon.style.fill = "none";
+			});
+
+			notifiIcon.forEach((icon) => {
+				icon.innerHTML =
+					'<svg aria-label="Activity Feed" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>';
+			});
+
+			searchIconDiv.innerHTML =
+				'<svg aria-label="Search &amp; Explore" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>';
+		} else {
+			showSignInModal();
+		}
+	}
+
+	function showSignInModal() {
+		const modal = document.getElementById("loginModal");
+		modal.style.display = "flex";
+	}
+
+	function searchIcon() {
+		const user = getAuth().currentUser;
+		const searchIconDiv = document.getElementById("search-icon-div");
+		const homeSvg = Array.from(document.getElementsByClassName("home-icon"));
+
+		homeSvg.forEach((icon) => {
+			icon.style.fill = "none";
+		});
+
+		searchIconDiv.innerHTML =
+			'<svg aria-label="Search &amp; Explore" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M47.6 44L35.8 32.2C38.4 28.9 40 24.6 40 20 40 9 31 0 20 0S0 9 0 20s9 20 20 20c4.6 0 8.9-1.6 12.2-4.2L44 47.6c.6.6 1.5.6 2.1 0l1.4-1.4c.6-.6.6-1.6.1-2.2zM20 35c-8.3 0-15-6.7-15-15S11.7 5 20 5s15 6.7 15 15-6.7 15-15 15z"></path></svg>';
+
+		if (user) {
+			const svgDiv = document.getElementById("message-icon-svg");
+			const notifiIcon = Array.from(document.getElementsByClassName("notification-svg"));
+
+			svgDiv.innerHTML =
+				'<line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line> <polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon>';
+
+			notifiIcon.forEach((icon) => {
+				icon.innerHTML =
+					'<svg aria-label="Activity" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>';
+			});
+		} else {
+		}
+	}
+
 	return (
 		<>
 			<nav id="top-nav">
 				<div className="nav-container">
 					<div className="nav-inner-container">
-						<div id="logo">Fakegram</div>
+						<div onClick={() => homeIcon()} id="logo">
+							Fakegram
+						</div>
 						<div id="search-div">
 							<input id="search-input" className="search-input" type="text" placeholder="Search" />
 						</div>
 						<div id="icon-div">
-							<div className="icon-inner-div icon-to-hide">
+							<div onClick={() => homeIcon()} className="icon-inner-div icon-to-hide">
 								<svg
 									aria-label="Home"
 									color="#262626"
@@ -23,8 +229,7 @@ function Nav() {
 									width="24"
 								>
 									<path
-										id="home-icon"
-										className="svh-path"
+										className="svh-path home-icon"
 										d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
 										fill="black"
 										stroke="currentColor"
@@ -33,8 +238,9 @@ function Nav() {
 									></path>
 								</svg>
 							</div>
-							<div className="icon-inner-div">
+							<div onClick={() => messageIcon()} className="icon-inner-div">
 								<svg
+									id="message-icon-svg"
 									aria-label="Share Post"
 									color="#262626"
 									fill="#262626"
@@ -104,7 +310,10 @@ function Nav() {
 									></line>
 								</svg>
 							</div>
-							<div className="icon-inner-div icon-to-hide">
+							<div
+								onClick={() => notificationIcon()}
+								className="icon-inner-div icon-to-hide notification-svg"
+							>
 								<svg
 									aria-label="Activity Feed"
 									color="#262626"
@@ -117,41 +326,14 @@ function Nav() {
 									<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
 								</svg>
 							</div>
-							<div className="icon-inner-div icon-to-hide">
-								{/* <span role="link" tabIndex="0" style={{ width: "24px", height: "24px" }}>
-							<img id="ava" crossOrigin="anonymous" draggable="false" src={ava} />
-						</span> */}
-								<svg
-									stroke="currentColor"
-									fill="currentColor"
-									strokeWidth="0"
-									viewBox="0 0 512 512"
-									height="24"
-									width="24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fill="none"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="40"
-										d="M344 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z"
-									></path>
-									<path
-										fill="none"
-										strokeMiterlimit="10"
-										strokeWidth="40"
-										d="M256 304c-87 0-175.3 48-191.64 138.6C62.39 453.52 68.57 464 80 464h352c11.44 0 17.62-10.48 15.65-21.4C431.3 352 343 304 256 304z"
-									></path>
-								</svg>
-							</div>
+							<div className="icon-inner-div icon-to-hide">{profileIcon()}</div>
 						</div>
 					</div>
 				</div>
 			</nav>
 			<nav id="bottom-nav">
 				<div className="nav-container-bottom">
-					<div className="icon-inner-div inner-bottom">
+					<div onClick={() => homeIcon()} className="icon-inner-div inner-bottom">
 						<svg
 							aria-label="Home"
 							color="#262626"
@@ -162,16 +344,16 @@ function Nav() {
 							width="24"
 						>
 							<path
-								className="svh-path"
+								className="svh-path home-icon"
 								d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
-								fill="none"
+								fill="black"
 								stroke="currentColor"
 								strokeLinejoin="round"
 								strokeWidth="2"
 							></path>
 						</svg>
 					</div>
-					<div className="icon-inner-div inner-bottom">
+					<div onClick={() => searchIcon()} id="search-icon-div" className="icon-inner-div inner-bottom">
 						<svg
 							aria-label="Search &amp; Explore"
 							color="#262626"
@@ -245,7 +427,7 @@ function Nav() {
 						</svg>
 					</div>
 
-					<div className="icon-inner-div inner-bottom">
+					<div onClick={() => notificationIcon()} className="icon-inner-div inner-bottom notification-svg">
 						<svg
 							aria-label="Activity Feed"
 							color="#262626"
@@ -258,34 +440,7 @@ function Nav() {
 							<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
 						</svg>
 					</div>
-					<div className="icon-inner-div inner-bottom">
-						{/* <span role="link" tabIndex="0" style={{ width: "24px", height: "24px" }}>
-							<img id="ava" crossOrigin="anonymous" draggable="false" src={ava} />
-						</span> */}
-						<svg
-							stroke="currentColor"
-							fill="currentColor"
-							strokeWidth="0"
-							viewBox="0 0 512 512"
-							height="24"
-							width="24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill="none"
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="40"
-								d="M344 144c-3.92 52.87-44 96-88 96s-84.15-43.12-88-96c-4-55 35-96 88-96s92 42 88 96z"
-							></path>
-							<path
-								fill="none"
-								strokeMiterlimit="10"
-								strokeWidth="40"
-								d="M256 304c-87 0-175.3 48-191.64 138.6C62.39 453.52 68.57 464 80 464h352c11.44 0 17.62-10.48 15.65-21.4C431.3 352 343 304 256 304z"
-							></path>
-						</svg>
-					</div>
+					<div className="icon-inner-div inner-bottom">{profileIcon()}</div>
 				</div>
 			</nav>
 		</>
