4331288d2769d280e22e20a93418f64bcc383a3f
igips <igor.szepietowski@gmail.com>
2022-03-28 21:57:24 +0200

    Move modals

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import "../styles/App.css";
 import Nav from "./Nav.js";
 import Home from "./Home.js";
-
+import Modals from "./Modals.js";
 
 
 function App() {
   return (
     <section id="top-section">
+      <Modals></Modals>
       <Nav></Nav>
       <Home></Home>
       
