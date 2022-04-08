7505d5a0a5a0c5f344a8baf6a2c373a177bf786f
igips <igor.szepietowski@gmail.com>
2022-04-08 22:59:59 +0200

    Change scope of function

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 import ReactTimeAgo from "react-time-ago";
+import { getUserDataFromUsersArray } from "./PictureCard";
 
 function showSignInModal() {
 	const modal = document.getElementById("login-modal");
@@ -118,6 +119,27 @@ function searchIcon() {
 	// }
 }
 
+function mobileNotificationIcon(e) {
+	const user = getAuth().currentUser;
+
+	const notiModal = document.getElementById("notification-modal-container");
+
+	if (user) {
+		notificationIconClicked();
+		messageIconNotClicked();
+		homeIconNotClicked();
+		searchIconNotClicked();
+		avatarIconNotClicked();
+		notiModal.style.display = "flex";
+		if (!window.location.href.includes("notiM")) {
+			window.history.pushState("notiM", "Title", "notiM");
+		}
+
+	} else {
+		showSignInModal();
+	}
+}
+
 function notificationIcon(e) {
 	const user = getAuth().currentUser;
 
@@ -193,7 +215,6 @@ function closeNotification() {
 	}
 	document.getElementById("notification-div").style.display = "none";
 	document.getElementById("notification-arrow-div").style.display = "none";
-	// document.getElementById("no-notifications").style.display = "none";
 }
 
 async function searchFunction(value) {
@@ -209,15 +230,36 @@ async function searchFunction(value) {
 	return results;
 }
 
-function Nav() {
+function followButtonForNoti(user, following, username, follow, unFollow) {
+	if (!following.includes(user) && user !== username) {
+		return (
+			<button
+				onClick={() => {
+					follow(user);
+				}}
+				className="likes-modal-follow"
+			>
+				Follow
+			</button>
+		);
+	} else if (following.includes(user)) {
+		return (
+			<button
+				onClick={() => {
+					unFollow(user);
+				}}
+				className="likes-modal-follow sug-box-left-follow-active"
+			>
+				Following
+			</button>
+		);
+	}
+}
+
+function Nav(props) {
 	const [avatar, setAvatar] = useState(0);
 	const [searchValue, setSearchValue] = useState("");
 	const [searchResults, setSearchResults] = useState([]);
-	const [notifications, setNotifications] = useState([
-		{ username: "igips", content: "started following you.", date: new Date() },
-	]);
-
-	
 
 	useEffect(() => {
 		window.addEventListener("click", (e) => {
@@ -248,13 +290,22 @@ function Nav() {
 	}, []);
 
 	useEffect(() => {
-		if(notifications.length === 0) {
-			
+		if (props.unReadNoti === 0) {
+			document.getElementById("noti-amount").style.display = "none";
+			document.getElementById("noti-amount-mobile").style.display = "none";
+		} else {
+			document.getElementById("noti-amount").style.display = "flex";
+			document.getElementById("noti-amount-mobile").style.display = "flex";
+		}
+	}, [props.unReadNoti]);
+
+	useEffect(() => {
+		if (props.notifications.length === 0) {
 			document.getElementById("no-notifications").style.display = "flex";
 		} else {
 			document.getElementById("no-notifications").style.display = "none";
 		}
-	}, [notifications])
+	}, [props.notifications]);
 
 	function cancelSearch() {
 		setSearchResults([]);
@@ -507,33 +558,38 @@ function Nav() {
 							</div>
 							<div
 								id="noti-icon"
-								onClick={(e) => notificationIcon(e)}
+								onClick={(e) => {
+									notificationIcon(e);
+									props.clearNotifications();
+								}}
 								className="icon-inner-div icon-to-hide icon-relative"
 							>
+								<div id="noti-amount">{props.unReadNoti}</div>
 								<div id="notification-arrow-div"></div>
 								<div id="notification-div">
-									<div id="no-notifications">
-										No notifications
-									</div>
-									{notifications.map((result) => {
-										return (
-											<div key={uniqid()} className="noti-modal-single-result">
-												<img src={ava} alt="" />
-												<span>{result.username}</span>
-												<span>&nbsp;</span>
-												<span className="noti-content">{result.content}</span>
-												<span>&nbsp;</span>
-												<span className="noti-date">
-													<ReactTimeAgo
-														timeStyle="mini-minute-now"
-														date={result.date}
-														locale="en-US"
-													/>
-												</span>
-												<button className="likes-modal-follow">Follow</button>
-											</div>
-										);
-									})}
+									<div id="no-notifications">No notifications</div>
+									{props.notifications
+										.slice(0)
+										.reverse()
+										.map((result) => {
+											return (
+												<div key={uniqid()} className="noti-modal-single-result">
+													<img src={ava} alt="" />
+													<span>{result.username}</span>
+													<span>&nbsp;</span>
+													<span className="noti-content">{result.content}</span>
+													<span>&nbsp;</span>
+													<span className="noti-date">
+														<ReactTimeAgo
+															timeStyle="mini-minute-now"
+															date={new Date(result.date)}
+															locale="en-US"
+														/>
+													</span>
+													{followButtonForNoti(result.username, props.following, props.username, props.follow, props.unFollow)}
+												</div>
+											);
+										})}
 								</div>
 								<div className="notification-svg svg-div">
 									<svg
@@ -650,18 +706,21 @@ function Nav() {
 						</svg>
 					</div>
 
-					<div onClick={() => notificationIcon()} className="icon-inner-div inner-bottom notification-svg">
-						<svg
-							aria-label="Activity Feed"
-							color="#262626"
-							fill="#262626"
-							height="24"
-							role="img"
-							viewBox="0 0 24 24"
-							width="24"
-						>
-							<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
-						</svg>
+					<div id="mobile-noti-icon" onClick={(e) => {mobileNotificationIcon(e); props.clearNotifications()}} className="icon-inner-div inner-bottom">
+						<div id="noti-amount-mobile"></div>
+						<div className="notification-svg svg-div">
+							<svg
+								aria-label="Activity Feed"
+								color="#262626"
+								fill="#262626"
+								height="24"
+								role="img"
+								viewBox="0 0 24 24"
+								width="24"
+							>
+								<path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
+							</svg>
+						</div>
 					</div>
 					<div className="icon-inner-div inner-bottom">{profileIcon()}</div>
 				</div>
@@ -671,4 +730,4 @@ function Nav() {
 }
 
 export default Nav;
-export { homeIcon, showSignInModal, searchFunction };
+export { homeIcon, showSignInModal, searchFunction, followButtonForNoti };
