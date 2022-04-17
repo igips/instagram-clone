7b1886d96ac976eb98a0ba3543efe0e2e36f7e8c
igips <igor.szepietowski@gmail.com>
2022-04-17 22:27:16 +0200

    Remove arrows

--- src/components/Icons/ArrowIcons.js
+++ src/components/Icons/ArrowIcons.js
@@ -1,48 +1,4 @@
-function LeftArrow() {
-	return (
-		<svg
-			aria-label="Left chevron"
-			color="#ffffff"
-			fill="#ffffff"
-			height="16"
-			role="img"
-			viewBox="0 0 24 24"
-			width="16"
-		>
-			<polyline
-				fill="none"
-				points="16.502 3 7.498 12 16.502 21"
-				stroke="currentColor"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth="2"
-			></polyline>
-		</svg>
-	);
-}
 
-function RightArrow() {
-	return (
-		<svg
-			aria-label="Right chevron"
-			color="#ffffff"
-			fill="#ffffff"
-			height="16"
-			role="img"
-			viewBox="0 0 24 24"
-			width="16"
-		>
-			<polyline
-				fill="none"
-				points="8 3 17.004 12 8 21"
-				stroke="currentColor"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth="2"
-			></polyline>
-		</svg>
-	);
-}
 
 function BackArrow() {
 	return (
@@ -78,4 +34,4 @@ function BackArrow() {
 	);
 }
 
-export { LeftArrow, RightArrow, BackArrow};
+export {BackArrow};
