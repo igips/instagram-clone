8ffa7a4dfdf19f8df0e739b1c3b25434a8b0344d
igips <igor.szepietowski@gmail.com>
2022-04-20 22:15:01 +0200

    Add icon

--- src/components/Icons/CommentIcon.js
+++ src/components/Icons/CommentIcon.js
@@ -12,5 +12,18 @@ function CommentIcon() {
 	);
 }
 
+function CommentIconFilled() {
+	return (
+		<svg aria-label="Comment" color="#ffffff" fill="#ffffff" height="18" role="img" viewBox="0 0 24 24" width="18">
+			<path
+				d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z"
+				fill="#ffffff"
+				stroke="currentColor"
+				strokeLinejoin="round"
+				strokeWidth="2"
+			></path>
+		</svg>
+	);
+}
 
-export {CommentIcon}
+export { CommentIcon, CommentIconFilled };
