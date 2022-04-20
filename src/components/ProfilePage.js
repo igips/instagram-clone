5748f417a0d1ce034ae5c76aa9141fb368ee5d0f
igips <igor.szepietowski@gmail.com>
2022-04-20 22:14:53 +0200

    Add change avatar function

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -6,26 +6,95 @@ import { getUserData } from "..";
 import { showLikesModal } from "./Modals/LikesModal";
 import { getUserDataFromUsersArray } from "./Home";
 import uniqid from "uniqid";
+import { LikeIconWhite } from "./Icons/LikeIcon";
+import { CommentIcon, CommentIconFilled } from "./Icons/CommentIcon";
+import { showCommentsModal } from "./Modals/CommentsModal";
+import { homeIconClicked, homeIconNotClicked } from "./Icons/HomeIcon";
+import { avatarIconClicked, avatarIconNotClicked } from "./Icons/ProfileIcon";
+import { deleteObject, getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { doc, getFirestore, updateDoc } from "firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function ProfilePage(props) {
 	let { username } = useParams();
 
 	const [userData, setUserData] = useState({ username: "", posts: [], followers: [], following: [] });
+	const [postsOrTagged, setPostsOrTagged] = useState("posts");
 
 	useEffect(() => {
-		if (props.users.length > 0 && username) {
-            const data = getUserDataFromUsersArray(props.users, username);
-            data.posts.sort((a, b) => {
-                return new Date(b.date) - new Date(a.date);
-            });
+		if (username === props.yourUsername) {
+			avatarIconClicked();
+		} else {
+			homeIconClicked();
+		}
+	});
 
+	useEffect(() => {
+		const posts = document.getElementById("profile-posts-span");
+		const tagged = document.getElementById("profile-tagged-span");
+		if (postsOrTagged === "posts") {
+			posts.style.color = "#262626";
+			tagged.style.color = "#8e8e8e";
+		} else {
+			posts.style.color = "#8e8e8e";
+			tagged.style.color = "#262626";
+		}
+	}, [postsOrTagged]);
+
+	useEffect(() => {
+		homeIconNotClicked();
+
+		if (props.users.length > 0 && username) {
+			const data = getUserDataFromUsersArray(props.users, username);
+			data.posts.sort((a, b) => {
+				return new Date(b.date) - new Date(a.date);
+			});
 
 			setUserData(data);
 		}
-		// getUserData(username).then((user) => {
-		// 	setUserData(user.data());
-		// });
+
+		const avatar = document.getElementById("profile-ava");
+
+		if (username === props.yourUsername) {
+			avatar.style.cursor = "pointer";
+			avatarIconClicked();
+		} else {
+			avatar.style.cursor = "default";
+			avatarIconNotClicked();
+		}
 	}, [username, props.users]);
+
+	useEffect(() => {
+		if (props.yourUsername === username && userData.username !== "" && userData.avatar !== props.avatar) {
+			setUserData({ ...userData, avatar: props.avatar });
+		}
+	}, [props.avatar]);
+
+	async function changeAvatar(e) {
+		const avatarSpinner = document.getElementById("avatar-spinner");
+
+		if (e.target.files[0]) {
+			avatarSpinner.style.display = "flex";
+			const storage = getStorage();
+
+			if (userData.avatar) {
+				await deleteObject(ref(storage, "avatars/" + username));
+			}
+
+			const avatar = e.target.files[0];
+			const reader = new FileReader();
+
+			reader.onload = () => {};
+			const picImagesRef = ref(storage, "avatars/" + username);
+			const snapshot = await uploadBytes(picImagesRef, avatar);
+
+			const url = await getDownloadURL(snapshot.ref);
+			await updateDoc(doc(getFirestore(), "usernames", props.firestoreDocId), { avatar: url });
+			avatarSpinner.style.display = "none";
+			e.target.value = "";
+		}
+	}
 
 	function buttons() {
 		function followUnfollow() {
@@ -69,12 +138,73 @@ function ProfilePage(props) {
 		}
 	}
 
+	function displayPosts() {
+		if (postsOrTagged === "posts") {
+			return userData.posts.map((post) => {
+				return (
+					<div onClick={() => showCommentsModal(post.id, props.commModalSetPostId)} key={uniqid()}>
+						<img style={{ aspectRatio: "1/1" }} src={post.pic.src} alt="" />
+						<div className="likes-and-comments-profile">
+							<span>
+								{LikeIconWhite()} {post.likes.num}
+							</span>
+							<span>
+								{CommentIconFilled()} {post.comments.length}
+							</span>
+						</div>
+					</div>
+				);
+			});
+		} else {
+			return props.posts.map((post) => {
+				let found = false;
+				for (let t of post.pic.tags) {
+					if (t.username === username) {
+						found = true;
+						break;
+					}
+				}
+				if (found) {
+					return (
+						<div onClick={() => showCommentsModal(post.id, props.commModalSetPostId)} key={uniqid()}>
+							<img style={{ aspectRatio: "1/1" }} src={post.pic.src} alt="" />
+							<div className="likes-and-comments-profile">
+								<span>
+									{LikeIconWhite()} {post.likes.num}
+								</span>
+								<span>
+									{CommentIconFilled()} {post.comments.length}
+								</span>
+							</div>
+						</div>
+					);
+				}
+			});
+		}
+	}
+
+	function avatarInput() {
+		if (username === props.yourUsername) {
+			return <input onChange={(e) => changeAvatar(e)} id="avatar-input" type="file" accept="image/*" />;
+		}
+	}
+
 	return (
 		<main>
 			<section id="profile-section">
 				<div id="profile-top">
 					<div id="profile-avatar-div">
-						<img id="profile-ava" src={ava} alt="" />
+						<label >
+							{avatarInput()}
+							<img id="profile-ava" src={userData.avatar ? userData.avatar : ava} alt="" />
+							<div id="avatar-spinner">
+								<FontAwesomeIcon
+									style={{ height: "40px", width: "40px" }}
+									icon={faSpinner}
+									className="fa-spin"
+								/>
+							</div>
+						</label>
 					</div>
 					<div id="profile-info">
 						<div id="profile-info-one">
@@ -84,7 +214,7 @@ function ProfilePage(props) {
 						<div id="profile-info-two">
 							<div className="profile-num-info">
 								<span className="profile-num-info-span">{userData.posts.length}</span>
-								<span>posts</span>
+								<span>Posts</span>
 							</div>
 							<div
 								onClick={() =>
@@ -98,7 +228,7 @@ function ProfilePage(props) {
 								className="profile-num-info cursor"
 							>
 								<span className="profile-num-info-span">{userData.followers.length}</span>
-								<span>followers</span>
+								<span>{userData.followers.length === 1 ? "Follower" : "Followers"}</span>
 							</div>
 							<div
 								onClick={() =>
@@ -112,17 +242,21 @@ function ProfilePage(props) {
 								className="profile-num-info cursor"
 							>
 								<span className="profile-num-info-span">{userData.following.length}</span>
-								<span>following</span>
+								<span>Following</span>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div id="profile-bottom">
-					<div id="profile-photo-div">
-						{userData.posts.map((post) => {
-							return <img key={uniqid()} style={{aspectRatio: "1/1"}} src={post.pic.src} alt="" />;
-						})}
+					<div id="posts-tag-div">
+						<span id="profile-posts-span" onClick={() => setPostsOrTagged("posts")}>
+							POSTS
+						</span>
+						<span id="profile-tagged-span" onClick={() => setPostsOrTagged("tagged")}>
+							TAGGED
+						</span>
 					</div>
+					<div id="profile-photo-div">{displayPosts()}</div>
 				</div>
 			</section>
 		</main>
