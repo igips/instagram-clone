fcf752e004f07d3f539add38d967cb00686e66ed
igips <igor.szepietowski@gmail.com>
2022-06-12 22:02:06 +0200

    Add reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+import usersPostsReducer from "../features/usersAndPostsSlice";
+import modalsReducer from "../features/modalsSlice";
 
 export default configureStore({
 	reducer: {
 		user: userReducer,
+		usersAndPosts: usersPostsReducer,
+		modals: modalsReducer,
+		
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+		serializableCheck: false,
+	})
 });
